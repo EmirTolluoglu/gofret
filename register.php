b2a2a7115cd3e55bbff542ce4f2a24659c672dc1
Emir <71454370+EmirTolluoglu@users.noreply.github.com>
2022-08-12 23:05:05 +0300

    register fln

--- register.php
+++ register.php
@@ -45,7 +45,7 @@
         right: 50%;
     }
 
-    main .motto {
+    /* main .motto {
         font-weight: 600;
         color: aliceblue;
         position: absolute;
@@ -53,7 +53,7 @@
         width: 100%;
         top: 8rem;
         font-size: 1.7rem;
-    }
+    } */
 
     /* @extend display-flex; */
 
@@ -380,121 +380,139 @@
         border-radius: 50%;
     }
 
+
     .part .container {
         height: 90%;
     }
-
-    #part2 {
-        transform: translateX(100%);
-    }
-
     #part1 {
-        transform: translateX(0);
+        transform: translateX(-50%);
+    }
+    #part2 {
+        transform: translateX(50%);
     }
 
+
+/* 
+gelmiş = transform: translate(50%, -50%) !important;
+gelmemiş =  transform: translate(-50%, -50%) !important;
+gitmiş = transform: translate(-150%, -50%) !important;
+
+*/
     @keyframes slideIn {
         0% {
-            transform: translateX(100%);
+            transform: translateX(50%)
         }
 
         100% {
-            transform: translateX(0);
+            transform: translateX(-50%);
         }
     }
 
     @keyframes slideOut {
         0% {
-            transform: translateX(0%);
+            transform: translateX(-50%);
         }
 
         100% {
-            transform: translateX(-100%);
+            transform: translateX(-150%);
         }
     }
+
     @keyframes slideIn2 {
         0% {
-            transform: translateX(-100%);
+            transform: translateX(-150%);
         }
 
         100% {
-            transform: translateX(0);
+            transform: translateX(-50%);
         }
     }
 
     @keyframes slideOut2 {
         0% {
-            transform: translateX(0%);
+            transform: translateX(-50%);
         }
 
         100% {
-            transform: translateX(100%);
+            transform: translateX(50%);
         }
+    }
+
+    .d-none {
+        display: none;
     }
     </style>
     <main>
         <div class="back">
-            <div id="part1" class="part h-100 w-100 position-absolute">
-                <h4 class="motto">Yeni bir sırrı<br> açığa çıkarmak<br> üzeresin</h4>
-                <div class="container d-flex justify-content-center align-items-center">
-                    <form action="src/sign.php" method="POST" class="register-form" id="register-form">
+            <div class="part h-100 w-100 position-absolute">
+                <form action="src/deneme.php" method="POST" class="register-form" id="register-form">
+
+                    <div id="part1" class="px-5 position-absolute w-100 start-50" style="top: 25%;">
+                        <h4 class="text-center text-white mb-5 fs-3">Yeni bir sırrı<br> açığa çıkarmak<br> üzeresin
+                        </h4>
                         <div class="form-group">
                             <label for="name"><i class="fa fa-user fa-xs ms-1 text-white"></i></label>
-                            <input type="text" name="name" id="name" placeholder="Your Name" />
+                            <input type="text" name="name" id="name" placeholder="Your Name" required/>
                         </div>
                         <div class="form-group">
                             <label for="email"><i class="fa fa-envelope fa-xs ms-1 text-white"></i></label>
-                            <input type="email" name="email" id="email" placeholder="Your Email" />
+                            <input type="email" name="email" id="email" placeholder="Your Email" required/>
                         </div>
                         <div class="form-group">
-                            <label for="pass"><i class="fa fa-lock fa-xs ms-1 text-white"></i></label>
-                            <input type="password" name="pass" id="pass" placeholder="Password" onChange="onChange()" />
+                            <label for="password"><i class="fa fa-lock fa-xs ms-1 text-white"></i></label>
+                            <input type="password" name="password" id="password" placeholder="Password" onChange="onChange()" required/>
                         </div>
                         <div class="form-group mb-1">
-                            <label for="pass"><i class="fa fa-lock fa-xs ms-1 text-white"></i></label>
+                            <label for="password"><i class="fa fa-lock fa-xs ms-1 text-white"></i></label>
                             <input type="password" name="confirm" id="confirm_password" placeholder="Confirm Password"
-                                onChange="onChange()" />
+                                onChange="onChange()" required/>
                         </div>
                         <div class="form-group form-button">
-                            <input onclick="gopart()" type="submit" name="signup" id="signup" class="form-submit" value="Kayıt Ol" />
+                            <div onclick="gopart()" class="form-submit text-center">Kayıt Ol</div>
                         </div>
                         <div class="btn-hr"></div>
-                        <a class="text-decoration-none" href="login.php"><div class="btn-reg py-2 mt-3 rounded-4 border-white bg-transparent w-100 text-center text-white">Bir hesabım var!</div></a>
-                    </form>
-                </div>
+                        <a class="text-decoration-none" href="login.php">
+                            <div
+                                class="btn-reg py-2 mt-3 rounded-4 border-white bg-transparent w-100 text-center text-white">
+                                Bir hesabım var!</div>
+                        </a>
+                    </div>
 
-            </div>
 
-            <div id="part2" class="part h-100 w-100 position-absolute">
-                <button onclick="repart()" class="text-decoration-none btn" style="border: 0; outline: 0;" href=""><i class="fa fa-angle-left fa-2x text-white m-3"></i></button>
-                <h4 class="motto mb-0">Aaa Kayıt<br> oluyorsun,<br><small class="fs-6">Öğrenci Misin?</small></h4>
-                <div class="container d-flex justify-content-center align-items-center">
-                    <form method="POST" class="register-form" id="register-form">
+                    <div id="part2" class="px-5 position-absolute w-100 start-50" style="top: 25%;">
+                        <div onclick="repart()" class="text-decoration-none position-absolute "
+                            style="border: 0; outline: 0; background: 0; top:1rem; left:1rem; ">
+                            <i class="fa fa-angle-left fa-2x text-white m-3"></i>
+                        </div>
+                        <h4 class="text-center text-white mb-5 fs-3">Aaa Kayıt<br> oluyorsun,<br><small
+                                class="fs-6">Öğrenci Misin?</small></h4>
                         <div class="form-group">
-                            <label for="name"><i class="fa fa-user fa-xs ms-1 text-white"></i></label>
-                            <input type="text" name="name" id="name" placeholder="Okuduğun Okul" />
+                            <label for="school"><i class="fa fa-user fa-xs ms-1 text-white"></i></label>
+                            <input type="text" name="school" id="school" placeholder="Okuduğun Okul"/>
                         </div>
                         <div class="form-group">
-                            <label for="email"><i class="fa fa-envelope fa-xs ms-1 text-white"></i></label>
-                            <input type="email" name="email" id="email" placeholder="Sınıf Numaran" />
+                            <label for="s_num"><i class="fa fa-envelope fa-xs ms-1 text-white"></i></label>
+                            <input type="text" name="s_num" id="s_num" placeholder="Sınıf Numaran" />
                         </div>
                         <div class="form-group">
-                            <label for="pass"><i class="fa fa-pen-to-square fa-xs ms-1 text-white"></i></label>
-                            <input type="text" name="pass" id="pass" placeholder="Öğrenci Belgesi" />
+                            <label for="s_document"><i class="fa fa-pen-to-square fa-xs ms-1 text-white"></i></label>
+                            <input type="file" name="s_document" id="s_document" placeholder="Öğrenci Belgesi" />
                             <div class="check"></div>
                         </div>
                         <div class="form-group mb-1">
-                            <label for="pass"><i class="fa fa-pen-to-square fa-xs ms-1 text-white"></i></label>
-                            <input type="text" name="pass" id="pass" placeholder="Akbil" />
+                            <label for="akbil"><i class="fa fa-pen-to-square fa-xs ms-1 text-white"></i></label>
+                            <input type="file" name="akbil" id="akbil" placeholder="Akbil" />
                             <div class="check"></div>
                         </div>
                         <div class="form-group form-button">
                             <input type="submit" name="signup" id="signup" class="form-submit"
                                 value="Başvuru gönder ve devam et" />
-                            <p class="text-center mt-2 mx-auto">Öğrenci olduğunu doğrulamak için bu bilgilere ihtiyacımız duyuyoruz en fazla 1 gün sürüyor.</p>
+                            <p class="text-center mt-2 mx-auto">Öğrenci olduğunu doğrulamak için bu bilgilere
+                                ihtiyacımız duyuyoruz en fazla 1 gün sürüyor.</p>
                         </div>
-                    </form>
-                </div>
+                    </div>
 
+                </form>
             </div>
         </div>
 
@@ -509,13 +527,13 @@
     <script>
     var part1 = document.getElementById('part1');
     var part2 = document.getElementById('part2');
-    
+
     function repart() {
         part1.style.animation = "slideIn2 1s forwards";
         part2.style.animation = "slideOut2 1s forwards";
     }
 
-    function gopart(){
+    function gopart() {
         part1.style.animation = "slideOut 1s forwards";
         part2.style.animation = "slideIn 1s forwards";
     }
@@ -523,7 +541,6 @@
 
 
     <script src="js/all.min.js"></script>
-    <script src="js/main.js"></script>
     <script src="js/plugins.js"></script>
     <script src="js/vendor/modernizr-3.11.2.min.js"></script>
 </body>
---
+++ src/deneme.php
@@ -0,0 +1,6 @@
+<?php 
+echo $_POST['name'];
+echo $_POST['email'];
+echo $_POST['school'];
+echo $_POST['s_num'];
+?>
